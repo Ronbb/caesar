b9b47941a2e9329271ebbc18398b3f88ea2f7d33
ronbb <ronbiaobiao@gmail.com>
2020-08-27 00:16:49 +0800

    Change debug flag to private one.

--- server/caesar-main/config/flag.go
+++ server/caesar-main/config/flag.go
@@ -3,8 +3,8 @@ package config
 import "flag"
 
 var (
-	// Debug If this program runs in debug mode.
-	Debug = flag.Bool("debug", false, `Set debug mode for more runtime information.
+	// debug If this program runs in debug mode.
+	debug = flag.Bool("debug", false, `Set debug mode for more runtime information.
 This option may be overwritten by config.`)
 	// ConfigFile Config File Path.
   ConfigFile = flag.String("ConfigFile", "caesar-main.yaml", "Load config from this file.")
