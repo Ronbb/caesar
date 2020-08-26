9cc5b73704a239a6d2e3a465d7806fa18a92fa11
ronbb <ronbiaobiao@gmail.com>
2020-08-27 00:18:36 +0800

    Encapsulate user info interface of database.

---
+++ server/caesar-main/db/users.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"context"
+	"log"
+
+	"github.com/ronbb/caesar/public/models/caesar"
+)
+
+// UserSignUp Add a user to db.
+func UserSignUp(user *caesar.UserRegistration) error {
+	_, err := usersCol.InsertOne(context.TODO(), user)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return err
+}
+
+// UserSignIn Check a user from db.
+func UserSignIn(user *caesar.Authentication) (*caesar.UserInfo, error) {
+	res := usersCol.FindOne(context.TODO(), user)
+	userInfo := &caesar.UserInfo{}
+	err := res.Decode(userInfo)
+	if err != nil {
+    log.Println(err.Error())
+    return nil, err
+	}
+	return userInfo, nil
+}
