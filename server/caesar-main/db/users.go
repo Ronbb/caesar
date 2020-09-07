591280bd0997a7a461ebd5b85e648a34283328ed
ronbb <ronbiaobiao@gmail.com>
2020-09-08 00:31:13 +0800

    UserSignUp check duplicate username

--- server/caesar-main/db/users.go
+++ server/caesar-main/db/users.go
@@ -2,18 +2,45 @@ package db
 
 import (
 	"context"
+	"errors"
 	"log"
 
 	"github.com/ronbb/caesar/public/models/caesar"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const (
+	username = "username"
 )
 
 // UserSignUp Add a user to db.
 func UserSignUp(user *caesar.UserRegistration) error {
-	_, err := usersCol.InsertOne(context.TODO(), user)
+	r := usersCol.FindOne(context.TODO(), bson.M{
+		username: user.Username,
+	})
+
+	userInfo := &caesar.UserInfo{}
+	err := r.Decode(userInfo)
+
+	if err == nil {
+    err = errors.New("duplicate username")
+		log.Println(err.Error())
+		return err
+  }
+
+	if err.Error() != mongo.ErrNoDocuments.Error() {
+		log.Println(err.Error())
+		return err
+	}
+
+	_, err = usersCol.InsertOne(context.TODO(), user)
 	if err != nil {
 		log.Println(err.Error())
+		return err
 	}
-	return err
+
+	return nil
 }
 
 // UserSignIn Check a user from db.
